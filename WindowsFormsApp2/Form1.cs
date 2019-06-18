bf28debe464df57b582c0c9b90b01bcba794a465
MTRAETTA <MTRAETTA@DESKTOP-AVB257F>
2019-06-18 17:36:18 +0200

    18062019 inserito logfile

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -109,36 +109,42 @@ namespace WindowsFormsApp2
 
     private static void DownloadRemoteImageFile(string uri, string fileName)
     {
-      HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-
-      //request.Proxy = WebRequest.DefaultWebProxy;
-      //request.Credentials = System.Net.CredentialCache.DefaultCredentials; ;
-      //request.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
-      HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                 
-      // Check that the remote file was found. The ContentType
-      // check is performed since a request for a non-existent
-      // image file might be redirected to a 404-page, which would
-      // yield the StatusCode "OK", even though the image was not
-      // found.
-      if ((response.StatusCode == HttpStatusCode.OK ||
-          response.StatusCode == HttpStatusCode.Moved ||
-          response.StatusCode == HttpStatusCode.Redirect) &&
-          response.ContentType.StartsWith("image", StringComparison.OrdinalIgnoreCase))
+      try
       {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
 
-        // if the remote file was found, download oit
-        using (Stream inputStream = response.GetResponseStream())
-        using (Stream outputStream = File.OpenWrite(fileName))
+        //request.Proxy = WebRequest.DefaultWebProxy;
+        //request.Credentials = System.Net.CredentialCache.DefaultCredentials; ;
+        //request.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+        // Check that the remote file was found. The ContentType
+        // check is performed since a request for a non-existent
+        // image file might be redirected to a 404-page, which would
+        // yield the StatusCode "OK", even though the image was not
+        // found.
+        if ((response.StatusCode == HttpStatusCode.OK ||
+            response.StatusCode == HttpStatusCode.Moved ||
+            response.StatusCode == HttpStatusCode.Redirect) &&
+            response.ContentType.StartsWith("image", StringComparison.OrdinalIgnoreCase))
         {
-          byte[] buffer = new byte[4096];
-          int bytesRead;
-          do
+
+          // if the remote file was found, download oit
+          using (Stream inputStream = response.GetResponseStream())
+          using (Stream outputStream = File.OpenWrite(fileName))
           {
-            bytesRead = inputStream.Read(buffer, 0, buffer.Length);
-            outputStream.Write(buffer, 0, bytesRead);
-          } while (bytesRead != 0);
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            do
+            {
+              bytesRead = inputStream.Read(buffer, 0, buffer.Length);
+              outputStream.Write(buffer, 0, bytesRead);
+            } while (bytesRead != 0);
+          }
         }
+      }
+      catch (Exception ex) {
+        log.Fatal(ex.Message);
       }
     }
 
